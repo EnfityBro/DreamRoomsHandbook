b7c3129a8ccfaf8edf842a91b2659e806e78d4bd
EnfityBro <149163323+EnfityBro@users.noreply.github.com>
2026-08-12 23:07:30 +0300

    fix: region detection api

--- js/region.js
+++ js/region.js
@@ -2,10 +2,10 @@ const ruLanguageRegions = ['RU', 'BY', 'UA', 'KZ', 'KG', 'TJ'];
 
 function detectRegion() {
     return new Promise((resolve) => {
-        fetch('https://ipwho.is/')
+        fetch('https://region-by-ip.enfitygames.workers.dev/')
             .then(res => res.json())
             .then(data => {
-                let countryCode = data.country_code;
+                let countryCode = data.country;
                 console.log(`User's region is ${countryCode}`);
 
                 if (ruLanguageRegions.includes(countryCode)) {
